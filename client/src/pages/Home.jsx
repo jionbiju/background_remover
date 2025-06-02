c1a87d20b9ed458ca93e5ed5f69664225b3097f2
jionbiju <jionbiju@gmail.com>
2025-06-02 21:45:14 +0530

    Slider component created

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Header'
 import Steps from '../components/Steps'
+import Slider from '../components/Slider'
 
 const Home = () => {
   return (
     <div>
       <Header/>
       <Steps/>
+      <Slider/>
     </div>
   )
 }
