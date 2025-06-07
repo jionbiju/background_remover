2b3919ea259307a2e048a5c36d38ef53d50115c6
jionbiju <jionbiju@gmail.com>
2025-06-07 20:26:20 +0530

    clerk hooks implemented

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import BuyCredit from './pages/BuyCredit'
 import Result from './pages/Result'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+
 const App = () => {
   return (
     <div className='min-h-screen bg-gray-100'>
       <Navbar/>
+      
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/result' element={<Result/>}/>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import { assets } from '../assets/assets';
 import { Link } from 'react-router-dom';
+import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
 const Navbar = () => {
+  const {openSignIn} = useClerk();
+  const {isSignedIn, user} = useUser();
   return (
     <div className='flex items-center justify-between mx-8  lg:mx-16 pt-6'>
         <Link to='/'><img width={100} className='w-22 sm:w-28 md:w-34 xl:w-40 ' src={assets.logo} alt="" /></Link>
-        <button className='bg-orange-500  text-white font-medium flex gap-1.5 items-center text-[13px]  sm:px-2.5 sm:py-1.5  
+        {
+          isSignedIn
+          ? <div >
+            <UserButton appearance={{
+              elements:{
+                userButtonAvatarBox:"w-12 h-12",
+              },
+            }}/>
+          </div>
+          :<button onClick={() => openSignIn({})} className='bg-orange-500  text-white font-medium flex gap-1.5 items-center text-[13px]  sm:px-2.5 sm:py-1.5  
         rounded-full hover:bg-red-600 md:px-2.5 md:py-2 px-1.5 py-1.5 lg:px-4 lg:py-2.5 xl:py-4 lg:text-[16px] transform ease-in-out duration-300'>
             Get Started
             <img className='w-3 sm:w-4' src={assets.arrow_icon} alt="" />
-        </button>
+          </button>
+        }
+        
     </div>
   )
 }
